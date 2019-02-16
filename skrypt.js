cbaebd6c9af2b9581598f28ae0bcf0841e498406
braiank <braiankreft@gmail.com>
2019-02-16 15:19:47 +0100

    demo memo

--- skrypt.js
+++ skrypt.js
@@ -1,48 +1,56 @@
-
-
 function oblicz()
 {
-    var x = document.getElementById("tak").value;
     var start = Date.now();
-    fib(x);
+    var x = document.getElementById("wyraz").value;
+   
+    var memofib = function(fibo)
+    {       
+        var n = 1;
+        var m = 1;
+        var t = 0;
+            if(!memofib.cachedResults[fibo])
+                {           
+                    if(x<0)
+                    {
+                        memofib.cachedResults[fibo] = document.getElementById("wartosc").innerHTML = "Nr wyrazu ciagu nie moze byc ujemny! ";
+                    }
+                    else if(x==0)
+                    {
+                        memofib.cachedResults[fibo] = document.getElementById("wartosc").innerHTML = "Wartosc wynosi: "+t;
+                    }
+                    else if(x==1 || x==2)
+                    {
+                        t = 1;
+                        memofib.cachedResults[fibo] = document.getElementById("wartosc").innerHTML = "Wartosc wynosi: "+t;
+                        
+                    }
+                    else if(x>2)
+                    {           
+                        for(i=1;i<=x-2;i++)
+                        {
+                            t = n + m;
+                            n = m;
+                            m = t
+                        }
+                        memofib.cachedResults[fibo] = document.getElementById("wartosc").innerHTML = "Wartosc wynosi: "+t;                                               
+                    }
+                    else
+                    {
+                        memofib.cachedResults[fibo] = document.getElementById("wartosc").innerHTML = "Nieznany blad";
+                    }                                  
+                }   
+        return memofib.cachedResults[fibo];
+    }
+    memofib.cachedResults = {};
     
+   memofib(x); 
+    alert(memofib(x));
+
     var stop = Date.now();
     var czas = stop - start;
     document.getElementById("czas").innerHTML = "Czas : "+czas;
-    
 }
 
 
-function fib(x)
-{
-    var n = 1;
-    var m = 1;
-    var t = 0;
-        if(x<0)
-        {
-            return document.getElementById("wartosc").innerHTML = "Nr wyrazu ciagu nie moze byc ujemny! ";
-        }
-        else if(x==0)
-        {
-            return document.getElementById("wartosc").innerHTML = "Wartosc wynosi: "+t;
-        }
-        else if(x==1 || x==2)
-        {
-            t = 1;
-            return document.getElementById("wartosc").innerHTML = "Wartosc wynosi: "+t;
-        }
-        else if(x>2)
-        {           
-            for(i=1;i<=x-2;i++)
-            {
-                t = n + m;
-                n = m;
-                m = t
-            }
-            return document.getElementById("wartosc").innerHTML = "Wartosc wynosi: "+t;
-        }
-        else
-        {
-            return document.getElementById("wartosc").innerHTML = "Nieznany blad";
-        }
-}
+
+
